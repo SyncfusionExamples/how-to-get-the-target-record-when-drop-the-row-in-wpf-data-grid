9fc101effc976d98e112867c0643a1387a40051e
Root\vijayarasan.sivanandham <vijayarasan.sivanandham@syncfusion.com>
2020-04-08 18:42:36 +0530

    How to get the target record when drop the row in WPF DataGrid(SfDataGrid)?

---
+++ SfDataGridDemo/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Syncfusion.Data;
+using Syncfusion.UI.Xaml.Grid;
+using Syncfusion.UI.Xaml.Grid.Helpers;
+using System.Windows;
+
+namespace SfDataGridDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            sfDataGrid.RowDragDropController.Drop += RowDragDropController_Drop;
+        }
+
+        private void RowDragDropController_Drop(object sender, GridRowDropEventArgs e)
+        {
+            var droppedIndex = (int)e.TargetRecord;
+
+            var rowIndex = this.sfDataGrid.ResolveToRowIndex(droppedIndex);
+
+            NodeEntry recordEntry = null;
+
+            if (this.sfDataGrid.View.TopLevelGroup != null)
+                recordEntry = this.sfDataGrid.View.TopLevelGroup.DisplayElements[this.sfDataGrid.ResolveToRecordIndex(rowIndex)];            
+            else
+                recordEntry = this.sfDataGrid.View.Records[this.sfDataGrid.ResolveToRecordIndex(rowIndex)];
+
+            var targetRecord = ((recordEntry as RecordEntry).Data as OrderInfo);
+            
+            txtDisplayRecord.Text = "OrderId : " + targetRecord.OrderID + "\nCustomerID : " + targetRecord.CustomerID + "\nCustomerName : " + targetRecord.CustomerName + "\nCountry : " + targetRecord.Country + "\nUnitPrice : " + targetRecord.UnitPrice + "\nRow Index :" + droppedIndex;
+        }
+    }
+}
+         
+   
+
